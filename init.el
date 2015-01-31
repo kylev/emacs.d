d84fbbb07cc075dfd912cbec42f314714c81e73e
Kyle VanderBeek <kylev@kylev.com>
2015-01-31 14:32:43 -0800

    Basics established.
    Derived from https://github.com/flyingmachine/emacs-for-clojure

---
+++ init.el
@@ -0,0 +1,34 @@
+(require 'package)
+(add-to-list 'package-archives 
+	     '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+             '("tromey" . "http://tromey.com/elpa/") t)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+
+;; Download the ELPA archive description if needed.
+;; This informs Emacs about the latest versions of all packages, and
+;; makes them available for download.
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; Automated package installation from list
+(defvar my-packages
+  '(;; Better lisp expressions
+    paredit
+    ;; Double-pretty clojure
+    clojure-mode
+    clojure-mode-extra-font-locking
+    ;; GIT!
+    magit
+    ))
+
+;; Handle strange OSX "launched from GUI" shell problems
+(if (eq system-type 'darwin)
+    (add-to-list 'my-packages 'exec-path-from-shell))
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
