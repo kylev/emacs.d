daac5aa8593b52ca4b322bb77e9d9748c7f187b5
Kyle VanderBeek <kylev@kylev.com>
2015-01-31 16:20:59 -0800

    Turn on clojure rainbows

--- customizations/setup-clojure.el
+++ customizations/setup-clojure.el
@@ -4,5 +4,8 @@
 ;; This makes moving wit camel case works a little easier
 (add-hook 'clojure-mode-hook 'subword-mode)
 
+;; Make with the color matching
+(add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
+
 ;; A little more syntax highlighting
 (require 'clojure-mode-extra-font-locking)
